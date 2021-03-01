3ccba6f146a9c1feab8cf73e3b9fcdec33d96426
rhirani1 <hirani.rikesh@digital.homeoffice.gov.uk>
2021-03-01 16:52:48 +0000

    Express app using govuk-react-jsx

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.hydrate(
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+
+    </React.StrictMode>,
+    document.getElementById('root')
+);
